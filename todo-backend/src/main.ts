041ad9d83929bfa0c6a28bec55da631d730b44f8
Devanshu Jha <99939860+Devanshu254@users.noreply.github.com>
2024-05-25 09:36:20 +0530

    Update main.ts

--- todo-backend/src/main.ts
+++ todo-backend/src/main.ts
@@ -26,6 +26,6 @@ async function bootstrap() {
 
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(process.env.PORT, '0.0.0.0');
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
